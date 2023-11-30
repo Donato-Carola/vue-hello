1111f603b96e326d9e70a6167566e31a0f9839a6
Donato-Carola <don94@live.it>
2023-11-30 16:37:24 +0100

    inserimento immagine

--- js/script.js
+++ js/script.js
@@ -3,13 +3,16 @@
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
 
- const {createApp} = Vue 
+
+const {createApp} = Vue 
 
 createApp({
   data() {
     return {
-      message:'CIAO',
-      imagine:'https://m.media-amazon.com/images/I/91M9VaZWxOL._AC_UF1000,1000_QL80_.jpg',
+      message:'Ciao sono un panda adorabile',
+      imagine:'img/panda.jpg',
+      messageTwo:'Finalmente è riucito a stampare l\'immagine, OLE\''
+      
     }
   }
 }).mount('#app')
