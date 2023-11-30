027fd86591ebdb089dd68294c800c6dab1e21aad
Donato-Carola <don94@live.it>
2023-11-30 13:48:31 +0100

    inserimento parola

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-import { createApp } from 'vue'
+//!Descrizione:
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
+
+ const {createApp} = Vue 
 
 createApp({
   data() {
     return {
-      count: 0
+      message:'CIAO',
+      imagine:'https://m.media-amazon.com/images/I/91M9VaZWxOL._AC_UF1000,1000_QL80_.jpg',
     }
   }
 }).mount('#app')
